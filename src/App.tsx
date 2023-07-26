1dd7022c2c5b9647bdaf27f7b21ef0064325f5e2
const <const9501.dev@gmail.com>
2023-07-26 23:23:34 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import {Button, Input, Modal, Table} from 'antd';
+import type {ColumnsType, TableProps} from 'antd/es/table';
+import {deleteUser, IUser, selectAllUsers, selectFilteredUsers} from "./store/usersSlice";
+import {useAppSelector} from "./hook/useAppSelector";
+import {DeleteFilled, DeleteOutlined, EditOutlined, SearchOutlined} from "@ant-design/icons";
+import {useAppDispatch} from "./hook/useAppDispatch";
 
-function App() {
+
+
+const App = () => {
+
+  const [search, setSearch] = useState<string>('')
+  const users = useAppSelector(selectAllUsers)
+  const filteredUsers = useAppSelector((state) => selectFilteredUsers(state, search))
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const dispatch = useAppDispatch()
+
+
+  const columns: ColumnsType<IUser> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      sorter: (a, b) => a.name.localeCompare(b.name)
+    },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      sorter: (a, b) => a.date.localeCompare(b.date)
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: 'Actions',
+      render: (user) => {
+        return (
+          <div className='actions-wrapper'>
+            <DeleteOutlined
+              className='action-icon'
+              title='Delete'
+              onClick={() => dispatch(deleteUser(user.id))}
+            />
+
+            <EditOutlined
+              onClick={showModal}
+              className='action-icon'
+              title='Edit'
+            />
+          </div>
+        )
+      }
+    }
+  ]
+
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+
+      <div className='header'>
+        <Input
+          size="large"
+          placeholder="large size"
+          onChange={(event) => setSearch(event.target.value)}
+          prefix={<SearchOutlined/>}
+          className='search-input'
+        />
+
+        <Button type="primary" size='large' onClick={showModal}>
+          Add new user
+        </Button>
+      </div>
+
+
+      <Table
+        columns={columns}
+        rowKey={'id'}
+        dataSource={filteredUsers}
+        pagination={false}
+        className='table'
+      />
+
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </div>
   );
 }
---
+++ src/store/usersSlice.ts
@@ -0,0 +1,96 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from 'uuid';
+
+export interface IUser {
+  id: string
+  name: string
+  date: string
+  age: number
+}
+
+const initialState: IUser[] = [
+  {
+    id: uuidv4(),
+    name: 'Kostya',
+    age: 23,
+    date: '30.05.2000'
+  },
+  {
+    id: uuidv4(),
+    name: 'Sveta',
+    age: 22,
+    date: '22.05.2001'
+  },
+  {
+    id: uuidv4(),
+    name: 'Egor',
+    age: 20,
+    date: '03.10.2000'
+  },
+  {
+    id: uuidv4(),
+    name: 'Mef',
+    age: 26,
+    date: '28.05.1992'
+  },
+  {
+    id: uuidv4(),
+    name: 'Motya',
+    age: 27,
+    date: '28.05.1997'
+  },
+  {
+    id: uuidv4(),
+    name: 'Yura',
+    age: 27,
+    date: '28.05.1998'
+  },
+  {
+    id: uuidv4(),
+    name: 'Kirya',
+    age: 29,
+    date: '28.05.1999'
+  },
+  {
+    id: uuidv4(),
+    name: 'Nastya',
+    age: 24,
+    date: '06.02.2001'
+  },
+  {
+    id: uuidv4(),
+    name: 'Anna',
+    age: 24,
+    date: '06.02.2004'
+  },
+
+]
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    addUser: (state, action: PayloadAction<IUser>) => {
+
+    },
+    deleteUser: (state, action: PayloadAction<string>) => {
+
+      const id = action.payload
+
+      return state.filter(user => user.id !== id)
+
+    },
+    editUser: (state, action: PayloadAction) => {
+
+    }
+  }
+})
+
+export default usersSlice.reducer
+
+export const selectAllUsers = (state: IUser[]): IUser[] => state
+export const selectFilteredUsers = (state: IUser[], search: string): IUser[] => {
+  return state.filter((user) => user.name.toLowerCase().includes(search))
+}
+
+export const {addUser, deleteUser, editUser} = usersSlice.actions
