f712fde5595a0fca6d938222de5a14d6d7512c30
const <const9501.dev@gmail.com>
2023-07-27 14:57:36 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import {useState} from "react";
-import {Button, Input, Modal, Table} from 'antd';
-import type {ColumnsType} from 'antd/es/table';
+import {useRef, useState} from "react";
+import {Button, Input, InputRef, Modal, Space, Table} from 'antd';
 import {deleteUser, IUser, selectFilteredUsers} from "./store/usersSlice";
 import {useAppSelector} from "./hook/useAppSelector";
 import {DeleteOutlined, EditOutlined, SearchOutlined} from "@ant-design/icons";
 import {useAppDispatch} from "./hook/useAppDispatch";
 import AddUserForm from "./components/AddUserForm";
-import dayjs from "dayjs";
+import {FilterConfirmProps} from "antd/es/table/interface";
 
+import type { ColumnType, ColumnsType } from 'antd/es/table';
+
+type DataIndex = keyof IUser;
 
 const App = () => {
-
 
   const [search, setSearch] = useState<string>('')
   const filteredUsers = useAppSelector((state) => selectFilteredUsers(state, search))
@@ -22,21 +23,132 @@ const App = () => {
   const [userChange, setUserChange] = useState<IUser>()
 
 
+
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
+
+
+
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: DataIndex,
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+
+
+
+  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<IUser> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    // render: (text) =>
+    //   searchedColumn === dataIndex ? (
+    //     text
+    //   ) : (
+    //     text
+    //   ),
+  });
+
   const columns: ColumnsType<IUser> = [
     {
       title: 'Name',
       dataIndex: 'name',
-      sorter: (a, b) => a.name.localeCompare(b.name)
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      ...getColumnSearchProps('name'),
     },
     {
       title: 'Date',
       dataIndex: 'date',
-      sorter: (a, b) => a.date.localeCompare(b.date)
+      sorter: (a, b) => a.date.localeCompare(b.date),
+      ...getColumnSearchProps('date'),
+
     },
     {
       title: 'Age',
       dataIndex: 'age',
       sorter: (a, b) => a.age - b.age,
+      ...getColumnSearchProps('age'),
+
     },
     {
       title: 'Actions',
@@ -64,13 +176,6 @@ const App = () => {
   ]
 
 
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleClose = () => {
-    setIsModalOpen(false);
-  };
 
   return (
     <div className='wrapper'>
